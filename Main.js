d57122499d4da6eb213e4b805478aabd4638644b
hholb <hholbroo@unca.edu>
2024-02-10 07:04:09 -0500

    Add overall description, move points and lines to own module
    Add basic description of how the project works.
    
    Refactor Point and LineSegment classes into their own module.
    
    Add functionality to resize the canvas to respond to changing window size.

--- Main.js
+++ Main.js
@@ -1,75 +1,4 @@
-import {Vector2D} from "./Vector.js";
-
-class Point {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    // Return a new Vector2D(x, y) resulting from the subtraction of p1 from p2.
-    static subtract(p1, p2) {
-        return new Vector2D(p2.x - p1.x, p2.y - p1.y);
-    }
-}
-
-class LineSegment {
-    constructor(point1, point2) {
-        this.point1 = point1;
-        this.point2 = point2;
-    }
-
-    intersectsWith(other) {
-        return this.getIntersectionPoint(other) !== undefined;
-    }
-
-    getIntersectionPoint(other) {
-        const x1 = this.point1.x;
-        const x2 = this.point2.x;
-        const y1 = this.point1.y;
-        const y2 = this.point2.y;
-
-        const x3 = other.point1.x;
-        const x4 = other.point2.x;
-        const y3 = other.point1.y;
-        const y4 = other.point2.y;
-
-        const v1 = Vector2D.normalize(Point.subtract(this.point1, this.point2));
-        const v2 = Vector2D.normalize(Point.subtract(other.point1, other.point2));
-
-        const v1CrossV2 = Vector2D.cross(v1, v2);
-        if (v1CrossV2 === 0) {
-            console.log("Vectors are collinear, no intersections possible.");
-            return;
-        }
-
-        // Solve:
-        // x1 + t1(x2 - x1) = x3 + t2(x4 - x3) and
-        // y1 + t1(y2 - y1) = y3 + t2(y4 - y3)
-        // to find t1 and t2 in terms of the points.
-        const t2Numerator = ((y1 - y3) * (x2 - x1) + (x3 - x1) * (y2 - y1)) / ((x2 - x1) * (y4 - y3));
-        const t2Denominator = 1 - ((x4 - x3) * (y2 - y1)) / ((x2 - x1) * (y4 - y3));
-        const t2 = t2Numerator / t2Denominator;
-        const t1 = ((x3 - x1) + t2 * (x4 - x3)) / (x2 - x1);
-
-        console.log(`t1: ${t1}, t2: ${t2}`);
-
-        // Check if t1 or t2 are outside the line segments.
-        if (t1 < 0 || t1 > 1) {
-            console.log("Intersection is outside of line segment.");
-            return;
-        }
-        if (t2 < 0 || t2 > 1) {
-            console.log("Intersection is outside of line segment.");
-            return;
-        }
-
-        // We have an intersections! plug t1 back into the parametric equations.
-        const xIntersect = x1 + t1 * (x2 - x1);
-        const yIntersect = y1 + t1 * (y2 - y1);
-        console.log(`Found intersect at Point(${xIntersect}, ${yIntersect}`);
-        return new Point(xIntersect, yIntersect);
-    }
-}
+import {Point, LineSegment} from "./PointsAndLines.js";
 
 let points = [];
 let lines = [];
@@ -83,8 +12,22 @@ function clearCanvas() {
     lines = [];
 }
 
+function resizeCanvas() {
+    const canvas = document.getElementById("main-canvas");
+    const displayWidth = canvas.clientWidth;
+    const displayHeight = canvas.clientHeight;
+    const needResize = canvas.width !== displayWidth || canvas.height !== displayHeight;
+
+    if (needResize) {
+        canvas.width = displayWidth;
+        canvas.height = displayHeight;
+    }
+
+    draw();
+}
+
 // Find intersections between all line segments on the canvas.
-function findIntersection() {
+function findIntersections() {
     if (lines.length < 2) {
         console.log('Not enough line segments to have any intersections.');
         return;
@@ -110,57 +53,80 @@ function findIntersection() {
     }
 }
 
-function drawPoint(event) {
+function createPoint(event) {
     const point = new Point(event.offsetX, event.offsetY);
+    console.log(`Creating point at: ${point.x}, ${point.y}`);
     points.push(point);
-
-    const canvas = document.getElementById("main-canvas");
-    const ctx = canvas.getContext('2d');
-
-    console.log(`Drawing point at: ${point.x}, ${point.y}`);
-    const path = new Path2D();
-    path.arc(point.x, point.y, 5, 0, 2 * Math.PI);
-    ctx.fill(path);
 }
 
-function drawLine() {
-    if (points.length !== 2) {
-        console.log("Not enough points to draw a line segment.");
+function drawPoints() {
+    const canvas = document.getElementById("main-canvas");
+    const ctx = canvas.getContext('2d');
+    for (const point of points) {
+        console.log(`Drawing Point(${point.x}, ${point.y}`);
+        const path = new Path2D();
+        path.arc(point.x, point.y, 5, 0, 2 * Math.PI);
+        ctx.fill(path);
+    }
+}
+
+function* makePointsGenerator() {
+    if (points.length < 2) return;
+    for (let i = 1; i < points.length; i += 2) {
+        yield {point1: points[i - 1], point2: points[i]};
+    }
+}
+
+function createLineSegments(event) {
+    if (points.length % 2 !== 0) {
+        console.log("Uneven number of points. Cannot determine where to create line segments.");
         return;
     }
+    lines = [];
+    const pointsIter = makePointsGenerator();
+    for (const {point1, point2} of pointsIter) {
+        const line = new LineSegment(point1, point2);
+        lines.push(line);
+    }
+}
 
+function drawLines() {
     const canvas = document.getElementById("main-canvas");
     const ctx = canvas.getContext('2d');
 
-    const point1 = points.shift();
-    const point2 = points.shift();
-    const line = new LineSegment(point1, point2);
-    lines.push(line);
-
-    console.log(`Drawing line segment between Point(${point1.x}, ${point1.y}) and Point(${point2.x}, ${point2.y})`);
-    const path = new Path2D();
-    path.moveTo(line.point1.x, line.point1.y);
-    path.lineTo(line.point2.x, line.point2.y);
-    ctx.stroke(path);
+    for (const line of lines) {
+        const point1 = line.point1;
+        const point2 = line.point2;
+        console.log(`Drawing line segment between Point(${point1.x}, ${point1.y}) and Point(${point2.x}, ${point2.y})`);
+        const path = new Path2D();
+        path.moveTo(line.point1.x, line.point1.y);
+        path.lineTo(line.point2.x, line.point2.y);
+        ctx.stroke(path);
+    }
 }
 
-function initialize() {
+function draw() {
+    drawPoints();
+    drawLines();
+}
+
+function main() {
     // setup button handlers
     const clearButton = document.getElementById("clear-canvas-button");
     clearButton.onclick = clearCanvas;
     const intersectionButton = document.getElementById("find-intersection-button");
-    intersectionButton.onclick = findIntersection;
+    intersectionButton.onclick = findIntersections;
 
     // setup canvas interaction handlers
     const canvas = document.getElementById("main-canvas");
-    canvas.addEventListener("pointerdown", drawPoint);
-    canvas.addEventListener("pointerup", drawLine);
-}
-
-function main() {
-    initialize();
+    canvas.addEventListener("pointerdown", createPoint);
+    canvas.addEventListener("pointerdown", draw);
+    canvas.addEventListener("pointerup", createLineSegments);
+    canvas.addEventListener("pointerup", drawLines);
+    window.addEventListener('resize', resizeCanvas);
 }
 
 window.onload = () => {
+    resizeCanvas();
     main();
 };
--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,4 +1,4 @@
 REMOTE_TARGET="hholbroo@arden.cs.unca.edu:/home/hholbroo/public_html/CSCI346.Spring2024/LinesAndIntersections"
 LOCAL_DIR="."
 
-rsync -av $LOCAL_DIR/index.html $LOCAL_DIR/styles.css $LOCAL_DIR/Main.js $LOCAL_DIR/Vector.js $REMOTE_TARGET/
+rsync -av $LOCAL_DIR/index.html $LOCAL_DIR/styles.css $LOCAL_DIR/Main.js $LOCAL_DIR/PointsAndLines.js $LOCAL_DIR/Vector.js $REMOTE_TARGET/
